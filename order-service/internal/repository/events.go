c1c1bebef997a50eba988486983751ed4ea108b8
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2026-05-03 19:27:35 +0300

    events repo mark and get events methods add

--- order-service/internal/repository/events.go
+++ order-service/internal/repository/events.go
@@ -3,8 +3,11 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/identicalaffiliation/oms-with-events/order-service/internal/models/domain"
 )
 
@@ -39,6 +42,75 @@ func (r *eventsRepository) createEventWithTx(ctx context.Context,
 		event.EventType, event.Payload)
 	if err != nil {
 		return fmt.Errorf("create event: %w", err)
+	}
+
+	return nil
+}
+
+func (r *eventsRepository) GetUnsentEvents(
+	ctx context.Context,
+	batchSize int,
+) ([]*domain.OrderEvent, error) {
+	query := `
+		SELECT
+			id,
+			order_id,
+			event_type,
+			payload,
+			sent_at,
+			created_at
+		FROM
+			order_events
+		WHERE
+			sent_at IS NULL
+		ORDER BY
+			created_at
+		LIMIT $1
+	`
+
+	rows, err := r.pool.QueryContext(ctx, query, batchSize)
+	if err != nil {
+		return nil, fmt.Errorf("select events: %w", err)
+	}
+
+	defer rows.Close()
+
+	var events []*domain.OrderEvent
+	for rows.Next() {
+		e := &domain.OrderEvent{}
+		var payload []byte
+		err := rows.Scan(&e.ID, &e.OrderID, &e.EventType,
+			&payload, &e.SentAt, &e.CreatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("scan row: %w", err)
+		}
+		e.Payload = payload
+		events = append(events, e)
+	}
+
+	return events, nil
+}
+
+func (r *eventsRepository) MarkEventAsSent(
+	ctx context.Context,
+	eventID uuid.UUID,
+) error {
+	query := `
+		UPDATE
+			order_events
+		SET
+			sent_at = $1
+		WHERE
+			id = $2
+	`
+
+	_, err := r.pool.ExecContext(ctx, query, time.Now().UTC(), eventID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return errors.New("event not found")
+		}
+
+		return fmt.Errorf("update event: %w", err)
 	}
 
 	return nil
